fb48bfd2279203a4699dbb552293176dab2fb7a5
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 00:45:19 +0530

    Update Tree.java

--- DSA_All/DSA_Daily_Practice_Codes/Day-04-Tree/Tree-Postorder/src/com/traversal/Tree.java
+++ DSA_All/DSA_Daily_Practice_Codes/Day-04-Tree/Tree-Postorder/src/com/traversal/Tree.java
@@ -11,10 +11,13 @@ public class Tree {
 	}
 
 	public void insert(int data) {
+		// Inserts a new node with the given data into the tree.
+		// If the data already exists, it will not be inserted.
 
 		Node newNode = new Node(data);
 
 		if (root == null) {
+			// If the root is null, assign the new node as the root
 			root = newNode;
 			return;
 		}
@@ -23,7 +26,7 @@ public class Tree {
 
 		while (true) {
 
-			// duplication
+			// Check for duplication, if found, return
 			if (data == temp.getData()) {
 				return;
 			}
@@ -31,6 +34,7 @@ public class Tree {
 			if (data < temp.getData()) {
 
 				if (temp.getLeft() == null) {
+					// If the left child is null, insert the new node
 					temp.setLeft(newNode);
 					return;
 				}
@@ -39,46 +43,65 @@ public class Tree {
 			} else {
 
 				if (temp.getRight() == null) {
+					// If the right child is null, insert the new node
 					temp.setRight(newNode);
 					return;
 				}
 
 				temp = temp.getRight();
 			}
-
 		}
-
 	}
 
 	public Node getRoot() {
+		// Returns the root node of the tree.
 		return root;
 	}
 
 	public void postorderNormal() {
-		
+		// Performs postorder traversal of the tree iteratively using a stack.
+
 		Node temp = root;
 		Stack<Node> stack = new Stack<>();
-		
-		//INCOMPLETE
-		//while() {
-			while(temp!=null) {
+
+		while (true) {
+			while (temp != null) {
+				// Traverse to the leftmost node and push each node to the stack
 				stack.push(temp);
-				temp=temp.getLeft();
+				temp = temp.getLeft();
 			}
-			
-			if(stack.pop().getRight()!=null)
-				temp = stack.pop().getRight();
-			
-			System.out.println(temp.getData()+" ");
-			
-		//}
-			
-		
-		
-		
+
+			if (stack.isEmpty()) {
+				// If the stack is empty, traversal is complete
+				return;
+			}
+
+			if (stack.peek().getRight() == null) {
+				// If the right child of the node at the top of the stack is null, visit the node
+				temp = stack.pop();
+				System.out.println(temp.getData() + " ");
+				
+				// Check if the node is the right child of the previous node
+				// If so, visit the previous node as well
+				while (!stack.isEmpty() && temp == stack.peek().getRight()) {
+					temp = stack.pop();
+					System.out.println(temp.getData() + " ");
+				}
+			}
+
+			if (!stack.isEmpty()) {
+				// If the stack is not empty, set the current node to the right child of the node at the top of the stack
+				temp = stack.peek().getRight();
+			} else {
+				// If the stack is empty, traversal is complete
+				temp = null;
+			}
+		}
 	}
 
 	public void postorderRec(Node node) {
+		// Performs postorder traversal of the tree recursively.
+
 		if (node == null)
 			return;
 
