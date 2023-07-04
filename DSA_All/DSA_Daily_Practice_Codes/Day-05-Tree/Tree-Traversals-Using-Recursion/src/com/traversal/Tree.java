ffade4beaff7cbdc071570f38b29dd0120bf5760
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 00:36:51 +0530

    Update Tree.java

--- DSA_All/DSA_Daily_Practice_Codes/Day-05-Tree/Tree-Traversals-Using-Recursion/src/com/traversal/Tree.java
+++ DSA_All/DSA_Daily_Practice_Codes/Day-05-Tree/Tree-Traversals-Using-Recursion/src/com/traversal/Tree.java
@@ -13,9 +13,11 @@ public class Tree {
 	}
 
 	public void insert(int data) {
-
+		// Create a new node with the given data
 		Node newNode = new Node(data);
+
 		if (root == null) {
+			// If the tree is empty, make the new node the root
 			root = newNode;
 			return;
 		}
@@ -24,43 +26,70 @@ public class Tree {
 
 		while (true) {
 			if (data < temp.getData()) {
+				// If the data is less than the current node's data, go to the left child
 				if (temp.getLeft() == null) {
+					// If the left child is null, insert the new node
 					temp.setLeft(newNode);
 					return;
 				}
 				temp = temp.getLeft();
 			} else {
+				// If the data is greater than or equal to the current node's data, go to the right child
 				if (temp.getRight() == null) {
+					// If the right child is null, insert the new node
 					temp.setRight(newNode);
 					return;
 				}
 				temp = temp.getRight();
 			}
 		}
-
 	}
 
 	public void inorder(Node node) {
+		// Perform an inorder traversal of the tree
+
 		if (node == null)
 			return;
+
+		// Visit the left subtree
 		inorder(node.getLeft());
+
+		// Visit the current node
 		System.out.println(node.getData() + " ");
+
+		// Visit the right subtree
 		inorder(node.getRight());
 	}
 
 	public void postorder(Node node) {
+		// Perform a postorder traversal of the tree
+
 		if (node == null)
 			return;
+
+		// Visit the left subtree
 		postorder(node.getLeft());
+
+		// Visit the right subtree
 		postorder(node.getRight());
+
+		// Visit the current node
 		System.out.println(node.getData() + " ");
 	}
 
 	public void preorder(Node node) {
+		// Perform a preorder traversal of the tree
+
 		if (node == null)
 			return;
+
+		// Visit the current node
 		System.out.println(node.getData() + " ");
+
+		// Visit the left subtree
 		preorder(node.getLeft());
+
+		// Visit the right subtree
 		preorder(node.getRight());
 	}
 
