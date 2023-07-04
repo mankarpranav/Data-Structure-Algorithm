5b0a0c385bf71c184356a20875963545a9daa3f9
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 00:40:20 +0530

    Update Tree.java

--- DSA_All/DSA_Daily_Practice_Codes/Day-04-Tree/Tree-Preorder/src/com/traversal/Tree.java
+++ DSA_All/DSA_Daily_Practice_Codes/Day-04-Tree/Tree-Preorder/src/com/traversal/Tree.java
@@ -11,10 +11,12 @@ public class Tree {
 	}
 
 	public void insert(int data) {
+		// Inserts a new node with the given data into the tree.
 
 		Node newNode = new Node(data);
 
 		if (root == null) {
+			// If the tree is empty, make the new node the root
 			root = newNode;
 			return;
 		}
@@ -23,61 +25,68 @@ public class Tree {
 
 		while (true) {
 
-			// duplication
+			// Check for duplication
 			if (data == temp.getData()) {
 				return;
 			}
 
 			if (data < temp.getData()) {
-
+				// If the data is less than the current node's data, go to the left child
 				if (temp.getLeft() == null) {
+					// If the left child is null, insert the new node
 					temp.setLeft(newNode);
 					return;
 				}
 
 				temp = temp.getLeft();
 			} else {
-
+				// If the data is greater than the current node's data, go to the right child
 				if (temp.getRight() == null) {
+					// If the right child is null, insert the new node
 					temp.setRight(newNode);
 					return;
 				}
 
 				temp = temp.getRight();
 			}
-
 		}
-
 	}
 
 	public void preorderNormal() {
+		// Performs a normal preorder traversal of the tree (iterative approach).
+		// Prints the node values in preorder traversal order.
 
 		Node temp = root;
 		Stack<Node> stack = new Stack<>();
 
 		while (temp != null || !stack.empty()) {
 			while (temp != null) {
-				System.out.println(temp.getData() + " ");
+				System.out.print(temp.getData() + " "); // Print the node value
 				stack.push(temp);
 				temp = temp.getLeft();
+				// Go to the left child until it's null
 			}
 			temp = stack.pop().getRight();
+			// After reaching the leftmost node, go to the right child
 		}
-
+		System.out.println(); // Add a newline character after traversal
 	}
 
 	public void preorderRec(Node node) {
+		// Performs a recursive preorder traversal of the tree.
+		// Prints the node values in preorder traversal order.
 
 		if (node == null) {
 			return;
 		}
 
-		System.out.print(node.getData() + " ");
+		System.out.print(node.getData() + " "); // Print the node value
 		preorderRec(node.getLeft());
 		preorderRec(node.getRight());
 	}
 
 	public Node getRoot() {
+		// Returns the root node of the tree.
 		return root;
 	}
 
