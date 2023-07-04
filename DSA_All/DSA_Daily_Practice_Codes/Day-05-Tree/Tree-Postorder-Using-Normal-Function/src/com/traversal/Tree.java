744d24f02769ffe197ca6a5b49e75a9ccc208c03
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 00:21:48 +0530

    Update Tree.java

--- DSA_All/DSA_Daily_Practice_Codes/Day-05-Tree/Tree-Postorder-Using-Normal-Function/src/com/traversal/Tree.java
+++ DSA_All/DSA_Daily_Practice_Codes/Day-05-Tree/Tree-Postorder-Using-Normal-Function/src/com/traversal/Tree.java
@@ -15,8 +15,10 @@ public class Tree {
 	}
 
 	public void insert(int data) {
-
+		// Create a new node with the given data
 		Node newNode = new Node(data);
+
+		// If the tree is empty, make the new node the root
 		if (root == null) {
 			root = newNode;
 			return;
@@ -24,26 +26,31 @@ public class Tree {
 
 		Node temp = root;
 
+		// Traverse the tree to find the appropriate position for insertion
 		while (true) {
 			if (data < temp.getData()) {
+				// If the data is less than the current node's data, go to the left child
 				if (temp.getLeft() == null) {
+					// If the left child is null, insert the new node
 					temp.setLeft(newNode);
 					return;
 				}
 				temp = temp.getLeft();
 			} else {
+				// If the data is greater than or equal to the current node's data, go to the right child
 				if (temp.getRight() == null) {
+					// If the right child is null, insert the new node
 					temp.setRight(newNode);
 					return;
 				}
 				temp = temp.getRight();
 			}
 		}
-
 	}
 
 	public void postorder() {
 
+		// Create a nested class Pair to store a node and its flag
 		class Pair {
 			Node node;
 			char flag;
@@ -59,21 +66,23 @@ public class Tree {
 
 		while (temp != null || !stack.empty()) {
 			while (temp != null) {
+				// Traverse to the leftmost node and push each node along with 'L' flag onto the stack
 				stack.push(new Pair(temp, 'L'));
 				temp = temp.getLeft();
 			}
 
 			Pair pair = stack.pop();
 			if (pair.flag == 'L') {
+				// If the flag is 'L', it means we have visited the left subtree
+				// Change the flag to 'R' and push the node back onto the stack
 				temp = pair.node.getRight();
 				pair.flag = 'R';
 				stack.push(pair);
-
 			} else {
-				System.out.println(pair.node.getData() + " ");
+				// If the flag is 'R', it means we have visited both the left and right subtrees
+				// Print the data of the node
+				System.out.print(pair.node.getData() + " ");
 			}
 		}
-
 	}
-
 }
