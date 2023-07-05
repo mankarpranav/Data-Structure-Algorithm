86fe918772a922ad1ffba0769628a89714ba5110
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 22:53:38 +0530

    Update Tree.java

--- DSA_All/DSA_Daily_Practice_Codes/Day-06-Tree/Tree-Postorder-Without-Stack/src/com/traversal/Tree.java
+++ DSA_All/DSA_Daily_Practice_Codes/Day-06-Tree/Tree-Postorder-Without-Stack/src/com/traversal/Tree.java
@@ -1,72 +1,77 @@
+// Package declaration
 package com.traversal;
 
+// Importing required classes
 import java.util.Stack;
 
+// Tree class
 public class Tree {
 
-	private Node root;
+    private Node root;
 
-	public Tree() {
-		this.root = null;
-	}
+    // Constructor
+    public Tree() {
+        this.root = null;
+    }
 
-	public boolean insert(int data) {
+    // Method to insert a node in the tree
+    public boolean insert(int data) {
+        Node newNode = new Node(data);
 
-		Node newNode = new Node(data);
+        if (root == null) {
+            root = newNode;
+        }
 
-		if (root == null) {
-			root = newNode;
-		}
+        Node temp = root;
 
-		Node temp = root;
+        while (true) {
+            // Checking if the data already exists in the tree
+            if (data == temp.getData()) {
+                return false;
+            }
 
-		while (true) {
+            if (data < temp.getData()) {
+                // If data is less than the current node's data, move to the left subtree
+                if (temp.getLeft() == null) {
+                    temp.setLeft(newNode);
+                    return true;
+                }
+                temp = temp.getLeft();
+            } else {
+                // If data is greater than the current node's data, move to the right subtree
+                if (temp.getRight() == null) {
+                    temp.setRight(newNode);
+                    return true;
+                }
+                temp = temp.getRight();
+            }
+        }
+    }
 
-			if (data == temp.getData()) {
-				return false;
-			}
+    // Method to perform postorder traversal and print the nodes
+    public void postorder() {
+        // Using two stacks for postorder traversal
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
 
-			if (data < temp.getData()) {
+        stack1.push(root);
 
-				if (temp.getLeft() == null) {
-					temp.setLeft(newNode);
-					return true;
-				}
-				temp = temp.getLeft();
-			} else {
+        // Iteratively traversing the tree
+        while (!stack1.isEmpty()) {
+            Node current = stack1.pop();
+            stack2.push(current);
 
-				if (temp.getRight() == null) {
-					temp.setRight(newNode);
-					return true;
-				}
-				temp = temp.getRight();
-			}
-		}
+            // Pushing left and right children of the current node to stack1
+            if (current.getLeft() != null)
+                stack1.push(current.getLeft());
+            if (current.getRight() != null)
+                stack1.push(current.getRight());
+        }
 
-	}
-
-	public void postorder() {
-
-		Stack<Node> s1 = new Stack<Node>();
-		Stack<Node> s2 = new Stack<Node>();
-
-		s1.push(root);
-
-		while (!s1.isEmpty()) {
-			Node t = s1.pop();
-			s2.push(t);
-
-			if (t.getLeft() != null)
-				s1.push(t.getLeft());
-			if (t.getRight() != null)
-				s1.push(t.getRight());
-
-		}
-
-		while (!s2.isEmpty()) {
-			Node t = s2.pop();
-			System.out.println(t.getData() + " ");
-		}
-	}
-
+        // Printing the nodes in postorder
+        while (!stack2.isEmpty()) {
+            Node node = stack2.pop();
+            System.out.print(node.getData() + " ");
+        }
+    }
 }
