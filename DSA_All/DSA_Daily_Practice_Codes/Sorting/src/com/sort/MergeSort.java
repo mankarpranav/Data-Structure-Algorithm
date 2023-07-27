0207be7f049c68a94cd268510e3f20812fb23c16
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-27 21:41:44 +0530

    Update MergeSort.java

--- DSA_All/DSA_Daily_Practice_Codes/Sorting/src/com/sort/MergeSort.java
+++ DSA_All/DSA_Daily_Practice_Codes/Sorting/src/com/sort/MergeSort.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class MergeSort {
 
+	// Method to merge two sub-arrays into a single sorted array
 	static void merge(int arr[], int start, int mid, int end) {
 
-		// calculating sizes of left and right sub-array
+		// Calculating sizes of left and right sub-arrays
 		int leftSize = mid - start + 1;
 		int rightSize = end - mid;
 
-		// declaring two sub-arrays
+		// Declaring two sub-arrays
 		int L[] = new int[leftSize];
 		int R[] = new int[rightSize];
 
-		// copying data to these two arrays
+		// Copying data to these two arrays
 		for (int i = 0; i < leftSize; i++) {
 			L[i] = arr[start + i];
 		}
@@ -23,10 +24,10 @@ public class MergeSort {
 			R[j] = arr[mid + 1 + j];
 		}
 
-		// declaring indices for two sub-arrays
+		// Declaring indices for two sub-arrays and the merged array
 		int i = 0, j = 0, t = start;
 
-		// inserting elements into the third merger array
+		// Merging elements into the third merged array in sorted order
 		while (i < leftSize && j < rightSize) {
 			if (L[i] < R[j]) {
 				arr[t] = L[i];
@@ -38,32 +39,37 @@ public class MergeSort {
 			t++;
 		}
 
-		// for left remaining elements
+		// For left remaining elements
 		while (i < leftSize) {
 			arr[t] = L[i];
 			i++;
 			t++;
 		}
 
-		// for right remaining elements
+		// For right remaining elements
 		while (j < rightSize) {
 			arr[t] = R[j];
 			j++;
 			t++;
 		}
-
 	}
 
+	// Method to perform Merge Sort on the input array
 	static void mergeSort(int arr[], int start, int end) {
 
 		if (start < end) {
 			int mid = start + (end - start) / 2;
+
+			// Recursively sort the left and right halves of the array
 			mergeSort(arr, start, mid);
 			mergeSort(arr, mid + 1, end);
+
+			// Merge the two sorted halves
 			merge(arr, start, mid, end);
 		}
 	}
 
+	// Method to print the sorted array
 	static void print(int arr[]) {
 		System.out.println("Sorting array using Merge Sort ->");
 		for (int i = 0; i <= arr.length - 1; i++) {
@@ -86,7 +92,9 @@ public class MergeSort {
 			arr[i] = sc.nextInt();
 		}
 
+		// Perform Merge Sort on the input array
 		mergeSort(arr, 0, arr.length - 1);
+		// Print the sorted array
 		print(arr);
 	}
 
